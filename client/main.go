3db0a45be973204cc7c48ec4ba88479f08beca7e
kubotake-desktop <keigo1003560@gmail.com>
2025-07-18 21:22:28 +0900

    Unary RPC

--- client/main.go
+++ client/main.go
@@ -2,27 +2,47 @@ package main
 
 import (
 	"awsomeProject/pb"
+	"context"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 var (
-	serverAddr = "localhost:50051"     // サーバーのアドレスとポート番号
+	serverAddr = "localhost:50051"
 
-	timeoutDuration = 10 * time.Second // gRPCリクエストのタイムアウト時間
-	timeSleep       = 1 * time.Second  // 通信の挙動確認用のリクエスト間のスリープ時間（待機時間）
+	timeoutDuration = 10 * time.Second
+	timeSleep       = 1 * time.Second
 )
 
+// Unary RPC
+// サーバーにtitleを送り、ファイルに存在するかの確認結果をAlbum型で受け取る関数
+func callGetAlbum(client pb.AlbumServiceClient, title string) {
+	// リクエストのタイムアウトを設定
+	ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
+	defer cancel()
+
+	// GetAlbumメソッドを呼び出してサーバーにリクエストを送り、レスポンスを受け取る
+	resp, err := client.GetAlbum(ctx, &pb.GetAlbumRequest{Title: title})
+	if err != nil {
+		log.Fatalf("client.GetAlbum failed: %v", err)
+	}
+
+	log.Printf("response: %v", resp.Album)
+}
+
 func main() {
-	conn, err := grpc.NewClient(serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials())) // gRPCクライアントを作成
+	conn, err := grpc.NewClient(serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	defer conn.Close()
-	client := pb.NewAlbumServiceClient(conn) // gRPCクライアントをAlbumServiceClientに変換
+	client := pb.NewAlbumServiceClient(conn)
 
-	// 各通信方式のリクエストを実行する
+	// callGetAlbumを実行
+	callGetAlbum(client, "Blue Train")
+	callGetAlbum(client, "Not Exist Title")
 }
--- server/main.go
+++ server/main.go
@@ -2,27 +2,65 @@ package main
 
 import (
 	"awsomeProject/pb"
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 )
 
 const (
-	filePath = "db/album.json" // アルバム情報が格納されたJSONファイルへのパス(後ほど追加します。)
-	port     = "50051"         // サーバーが待ち受けるポート番号
+	filePath = "db/album.json" // JSONファイルに保存されたアルバムデータのパス
+	port     = "50051"
 )
 
-// pb.AlbumServiceServerインターフェースを満たすサーバーを定義する
-// 未実装のメソッドはpb.UnimplementedAlbumServiceServerのメソッドが使用される
 type AlbumServer struct {
 	pb.UnimplementedAlbumServiceServer
+
+	savedAlbums []*pb.Album // サーバーに保存されたアルバムのリスト
+}
+
+// Unary RPC
+// クライアントから送信されたアルバムのタイトルに基づいて、アルバム情報を返すメソッド
+func (s *AlbumServer) GetAlbum(ctx context.Context, req *pb.GetAlbumRequest) (*pb.GetAlbumResponse, error) {
+	for _, album := range s.savedAlbums {
+		if album.Title == req.Title {
+			log.Printf("album found: %s", req.Title)
+			return &pb.GetAlbumResponse{Album: album}, nil
+		}
+	}
+
+	log.Printf("album not found: %s", req.Title)
+	return &pb.GetAlbumResponse{Album: &pb.Album{}}, nil
+}
+
+// サーバーの初期化時にアルバムデータをロードするメソッド
+func (s *AlbumServer) loadAlbums(filePath string) error {
+	var (
+		data []byte
+		err  error
+	)
+	data, err = os.ReadFile(filePath) // ファイルからアルバム情報を読み取る
+	if err != nil {
+		return err
+	}
+
+    // JSONデータをGoの構造体に変換しsavedAlbumsに保存する
+	if err := json.Unmarshal(data, &s.savedAlbums); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func newServer() *AlbumServer {
 	s := &AlbumServer{}
-    // 後ほど初期化時に実行する処理を追加します。
+	if err := s.loadAlbums(filePath); err != nil { // サーバー起動時にアルバムデータをロード
+		log.Fatalf("failed to load albums: %v", err)
+	}
 
 	return s
 }
